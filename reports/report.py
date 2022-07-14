62490866f4000153fdb54a61995665b64a5f6286
Krona Emmanuel <kronaemmanuel@gmail.com>
2022-07-14 14:39:01 +0500

    Resetting to basics

--- reports/report.py
+++
@@ -1,9 +0,0 @@
-from data_manager import DataManager
-
-
-class Report:
-    def __init__(self, data_store: DataManager) -> None:
-        self.data_store = data_store
-
-    def printReport(self):
-        pass
